6ae80da084b9ec598008cffe9c45c8dc7dbdb61d
kondratskyivlad <55400806+kondratskyivlad@users.noreply.github.com>
2021-03-05 10:54:47 +0200

    init

---
+++ components/movie.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import {StyleSheet, View, Dimensions, Switch, Text} from 'react-native';
+
+const Movie = () => {
+    return (
+        <View>
+            <Text>Hello Movie</Text>
+        </View>
+    )
+}
+
+export default  Movie
+
